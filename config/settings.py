c42255789ef54d4a4642b9d44617fe31a46ddd3b
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-11-28 23:03:19 +0300

    step 4

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'milkut56466'
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,12 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-# Отвечает за формирование адреса доступа к статике
 STATIC_URL = 'static/'
-# Отвечает за место на диске, откуда необходимо подгружать статику
-STATICFILES_DIRS = (
+STATICFILES_DIR = (
     BASE_DIR / 'static',
 )
+
+MEDIA_URL = 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 
 # Default primary key field type
