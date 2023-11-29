c86f2566a657ba7a9431d158188b643ecbab9d88
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-11-29 22:17:51 +0300

    step 5

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='media/', null=True, blank=True, verbose_name='изображение')
+    category = models.ForeignKey
+    price = models.IntegerField()
+    date_base = models.DateField(null=True, blank=True, verbose_name='дата создания')
+    data_change = models.DateField(null=True, blank=True, verbose_name='дата изменения')
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
--- config/settings.py
+++ config/settings.py
@@ -125,8 +125,8 @@ STATICFILES_DIR = (
     BASE_DIR / 'static',
 )
 
-MEDIA_URL = 'media'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media',
 
 
 
