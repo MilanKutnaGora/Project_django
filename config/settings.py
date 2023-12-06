2ccdad389ff34149663786d66678995a9da44aba
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-12-07 00:06:42 +0300

    step 9 add management

--- config/settings.py
+++ config/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = (
-    BASE_DIR / 'static',
-)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
