acc617831e47b106c8deb29607742f2a157cbf4b
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-12-14 23:50:19 +0300

    step 17 change

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(verbose_name='описание')
-    image = models.ImageField(upload_to='media/', null=True, blank=True, verbose_name='изображение')
+    image = models.ImageField(upload_to='media/',  verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
                                  max_length=20, verbose_name='наименование категории')
     price = models.IntegerField(verbose_name='цена за покупку')
@@ -29,8 +29,8 @@ class Product(models.Model):
         return f"{self.name}"
 
     class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
 
 
 
