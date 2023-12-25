84c1a46dd31970cff67862597a6f87a1985adb86
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-12-25 06:51:17 +0300

    step 1 GRUD with forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import index, index_contacts, ProductListView, ProductDetailView
+from catalog.views import index, index_contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('contacts/', index_contacts, name='index_contact'),
     path('shop/', ProductListView.as_view(), name='index_shop'),
     path('shop/product/<int:pk>/', ProductDetailView.as_view(), name='index_product'),
+    path('shop/create/', ProductCreateView.as_view(), name='create_product'),
+    path('shop/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
 ]
