a838e05b68edeb1bca93d8fa675ff5fbd1d8215c
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2024-01-11 01:16:54 +0300

    step 2

--- config/settings.py
+++ config/settings.py
@@ -140,5 +140,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
-LOGOUT_REDIRECT_URL = '/',
-LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'user/login/'
